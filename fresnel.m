11c7672ff8222b309d3cbdbf16e2c2733318eedb
Wenhua Zhu <mlwdjq@lbl.gov>
2022-04-10 23:11:53 -0700

    bug fixed for fresnel equation

--- fresnel.m
+++ fresnel.m
@@ -4,4 +4,4 @@ th1 = asin((n0).*sin(th0)./(n1));
 rs = (n0.*cos(th0) - n1.*cos(th1))./(n0.*cos(th0) + n1.*cos(th1));
 rp = (n1.*cos(th0) - n0.*cos(th1))./(n1.*cos(th0) + n0.*cos(th1));
 ts = 1 + rs;
-tp = 1 - rp;
+tp = (1 + rp).*n0./n1;
